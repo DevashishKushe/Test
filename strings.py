eca82794a619e8b4de98c83b49938a1dcdbadd0e
DevashishKushe <devashishkushe@gmail.com>
2022-06-14 15:18:43 +0530

    Strings, formatted strings, string methods, use of 'in' operator

---
+++ strings.py
@@ -0,0 +1,16 @@
+first ='John'
+last= 'Smith'
+message = first + ' [' + last + '] is a coder' #string
+msg = f'{first} [{last}] is a coder' #formatted string
+print(message)
+print(msg)
+
+course='Python for beginners'
+print(course)
+print('Python' in course)
+course.upper()
+course.lower()
+course.title()
+course.find()
+course.replace()
+'...'in course
