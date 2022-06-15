9ef8d1df91e55a3a9254589bb276df1bdaa8b13f
DevashishKushe <devashishkushe@gmail.com>
2022-06-15 16:19:44 +0530

    if statements

---
+++ if_statements.py
@@ -0,0 +1,10 @@
+price=100000
+is_good_credit=True
+if is_good_credit:
+    down_payment=is_good_credit*(10/100)
+else:
+    down_payment=is_good_credit*(20/100)
+
+print(f"down payment= {down_payment}")
+
+
